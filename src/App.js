71146dc53eda6fa9be0c21738e8585a6e90cd61c
Polina Filippova <polina17012001@mail.ru>
2021-05-02 20:53:11 +0300

    Added version1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+import Rate from './Rate/Rate.js';
+import About from './About/About.js';
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="site">
+        <Header/>
+        <div className="container">
+          <main>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Rate} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </Router>
+          </main>
+        </div>
+
+        <div className="container" id="cookie_info">
+          <div className="site-content">
+            <div className="well">На нашем сайте мы используем cookie для сбора информации технического характера. <br/> 
+            В частности мы обрабатываем Ip адрес вашего региона.</div>
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
